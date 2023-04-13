7b4177e0878b5b3050e3214657c23dee91cc780e
Rama Fall <rama.fall49@gmail.com>
2023-04-13 13:50:37 +0000

    integration2

--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -5,7 +5,7 @@ const Product = () =>{
             <h1 className="feat">Featured Products</h1>
             <p className="blem">Problems trying to resolve the conflicts between <br /> the two major realms of Classical physics: Newtonian mechanics</p>
             <input type="text" placeholder="Your Email" className="form-control"/>
-            <button className="cribe p-3">Subscribe</button>
+            <button className="cribe p-3 mt-5">Subscribe</button>
         </section>
     )
 }
